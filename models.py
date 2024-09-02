9e5277d470c45dfc66092fc1acb3ffa5974c3736
guli <guli@tsinghua.org.cn>
2024-09-03 07:19:02 +0800

    fix

--- models.py
+++ models.py
@@ -927,7 +927,7 @@ class SynthesizerEval(nn.Module):
             print('no nead steam')
             one_time_wav = self.dec(z, g=g)[0, 0].data.cpu().float().numpy()
             #return one_time_wav
-            yield one_time_wav, -1, one_time_wav
+            yield one_time_wav, -1, [one_time_wav]
             return
 
         # can not change these parameters
